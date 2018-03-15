cfa5f147d52f506d0bc9c1cb63b1a7465b7d72f2
Dominik Ehrenberg <doe@crosscan.com>
2018-03-15 12:06:48 +0100

    fix examples

--- examples/app.js
+++ examples/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['tooltips']).controller('AppCtrl', function($scope) {
+angular.module('app', ['ng-tooltips']).controller('AppCtrl', function($scope) {
     $scope.tooltip = '<strong>Bold text</strong><br>And an icon<br><i class="fa fa-star"><i>';
     $scope.compile = 'asdf';
 
